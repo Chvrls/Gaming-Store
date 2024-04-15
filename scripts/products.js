783c1c7756df4989b5743c617f7d567df5af8e0c
Chvrls <charlesmabunga@gmail.com>
2024-04-15 16:23:56 +0800

    Adding Products to Store

--- scripts/products.js
+++ scripts/products.js
@@ -18,6 +18,33 @@ const products = [
     price: 40000,
   },
   {
+    image: "images/products/steam-deck-img.png",
+    name: "Steam Deck",
+    rating: {
+      stars: 5,
+      count: 104
+    },
+    price: 21795,
+  },
+  {
+    image: "images/products/computer-product.png",
+    name: "Essential Gaming PC",
+    rating: {
+      stars: 4.5,
+      count: 55
+    },
+    price: 40000,
+  },
+  {
+    image: "images/products/computer-product-2.png",
+    name: "Monster Gaming PC",
+    rating: {
+      stars: 5,
+      count: 96
+    },
+    price: 80000,
+  },
+  {
     image: "images/products/computer-product.png",
     name: "Essential Gaming PC",
     rating: {
@@ -34,5 +61,86 @@ const products = [
       count: 55
     },
     price: 40000,
-  }
+  },
+  {
+    image: "images/products/computer-product.png",
+    name: "Essential Gaming PC",
+    rating: {
+      stars: 4.5,
+      count: 55
+    },
+    price: 40000,
+  },
+  {
+    image: "images/products/computer-product-2.png",
+    name: "Monster Gaming PC",
+    rating: {
+      stars: 5,
+      count: 96
+    },
+    price: 80000,
+  },
+  {
+    image: "images/products/computer-product.png",
+    name: "Essential Gaming PC",
+    rating: {
+      stars: 4.5,
+      count: 55
+    },
+    price: 40000,
+  },
+  {
+    image: "images/products/computer-product.png",
+    name: "Essential Gaming PC",
+    rating: {
+      stars: 4.5,
+      count: 55
+    },
+    price: 40000,
+  },
+  {
+    image: "images/products/computer-product.png",
+    name: "Essential Gaming PC",
+    rating: {
+      stars: 4.5,
+      count: 55
+    },
+    price: 40000,
+  },
+  {
+    image: "images/products/computer-product-2.png",
+    name: "Monster Gaming PC",
+    rating: {
+      stars: 5,
+      count: 96
+    },
+    price: 80000,
+  },
+  {
+    image: "images/products/computer-product.png",
+    name: "Essential Gaming PC",
+    rating: {
+      stars: 4.5,
+      count: 55
+    },
+    price: 40000,
+  },
+  {
+    image: "images/products/computer-product.png",
+    name: "Essential Gaming PC",
+    rating: {
+      stars: 4.5,
+      count: 55
+    },
+    price: 40000,
+  },
+  {
+    image: "images/products/computer-product.png",
+    name: "Essential Gaming PC",
+    rating: {
+      stars: 4.5,
+      count: 55
+    },
+    price: 40000,
+  },
 ];
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,35 @@
 let productsHTML = '';
 
 products.forEach((product) => {
-  productsHTML += ``
+  productsHTML += 
+  `
+  <div class="product-container">
+    <div class="image-container">
+      <img class="product-img" src=${product.image} alt="">
+    </div>
 
-})
+    <div class="product-name">
+      ${product.name}
+    </div>
+
+    <div class="rating-container">
+      <img class="rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png" alt="">
+      <div class="rating-count">
+        ${product.rating.count}
+      </div>
+    </div>
+
+    <div class="product-price">
+      ₱ ${product.price}
+    </div>
+
+    <button class="add-to-cart">
+      Add to Cart
+    </button>
+
+  </div>
+  `;
+
+});
+
+document.querySelector('.product-grid').innerHTML = productsHTML;
