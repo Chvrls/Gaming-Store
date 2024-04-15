add4149f2ad2469d7026454202942fa02d085427
Chvrls <charlesmabunga@gmail.com>
2024-04-15 18:25:36 +0800

    Finished the Store Section

--- scripts/products.js
+++ scripts/products.js
@@ -27,120 +27,120 @@ const products = [
     price: 21795,
   },
   {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
+    image: "images/products/xbox-img.png",
+    name: "Xbox One",
     rating: {
       stars: 4.5,
-      count: 55
+      count: 1075
     },
-    price: 40000,
+    price: 12878,
   },
   {
-    image: "images/products/computer-product-2.png",
-    name: "Monster Gaming PC",
+    image: "images/products/ps5-img.png",
+    name: "PlayStation 5",
     rating: {
       stars: 5,
-      count: 96
+      count: 3021
     },
-    price: 80000,
+    price: 30790,
   },
   {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
+    image: "images/products/switch-img.png",
+    name: "Ninentendo Switch",
+    rating: {
+      stars: 4,
+      count: 345
+    },
+    price: 16995,
+  },
+  {
+    image: "images/products/asus-rog-img.png",
+    name: "Asus ROG Laptop",
+    rating: {
+      stars: 4,
+      count: 1125
+    },
+    price: 73975,
+  },
+  {
+    image: "images/products/logi-mouse-img.png",
+    name: "Logitech G402",
     rating: {
       stars: 4.5,
-      count: 55
+      count: 205
     },
-    price: 40000,
+    price: 1799,
   },
   {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
+    image: "images/products/razer-wired-img.png",
+    name: "Razer Wired",
     rating: {
-      stars: 4.5,
-      count: 55
+      stars: 3.5,
+      count: 81
     },
-    price: 40000,
+    price: 1450,
   },
   {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
-    rating: {
-      stars: 4.5,
-      count: 55
-    },
-    price: 40000,
-  },
-  {
-    image: "images/products/computer-product-2.png",
-    name: "Monster Gaming PC",
+    image: "images/products/viper-ultimate.png",
+    name: "Razer Viper Ultimate",
     rating: {
       stars: 5,
-      count: 96
+      count: 14
     },
-    price: 80000,
+    price: 10397,
   },
   {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
-    rating: {
-      stars: 4.5,
-      count: 55
-    },
-    price: 40000,
-  },
-  {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
-    rating: {
-      stars: 4.5,
-      count: 55
-    },
-    price: 40000,
-  },
-  {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
-    rating: {
-      stars: 4.5,
-      count: 55
-    },
-    price: 40000,
-  },
-  {
-    image: "images/products/computer-product-2.png",
-    name: "Monster Gaming PC",
+    image: "images/products/g403-img.png",
+    name: "Logitech G403",
     rating: {
       stars: 5,
-      count: 96
+      count: 344
     },
-    price: 80000,
+    price: 2271,
   },
   {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
+    image: "images/products/nitro-7-img.png",
+    name: "Acer Nitro 7",
     rating: {
-      stars: 4.5,
-      count: 55
+      stars: 3.5,
+      count: 11
     },
-    price: 40000,
+    price: 79999,
   },
   {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
+    image: "images/products/4070-img.png",
+    name: "Geforce RTX 4070",
     rating: {
-      stars: 4.5,
-      count: 55
+      stars: 5,
+      count: 21
     },
-    price: 40000,
+    price: 53499,
   },
   {
-    image: "images/products/computer-product.png",
-    name: "Essential Gaming PC",
+    image: "images/products/2060-img.png",
+    name: "Geforce RTX 2060 Super",
+    rating: {
+      stars: 5,
+      count: 87
+    },
+    price: 20905,
+  },
+  {
+    image: "images/products/keyboard-img.png",
+    name: "Logitech G Pro Keyboard",
     rating: {
       stars: 4.5,
-      count: 55
+      count: 36
     },
-    price: 40000,
+    price: 7015,
+  },
+  {
+    image: "images/products/headset-img.png",
+    name: "Logitech G Pro Headset",
+    rating: {
+      stars: 4,
+      count: 153
+    },
+    price: 7195,
   },
 ];
